d5b8e8bed89be0177a3e2fad15292fb42169c938
Kise <corinderria@gmail.com>
2019-09-16 16:42:51 +0300

    version5

---
+++ WebApplication2/WebApplication2/Scripts/Main.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $("#html").addClass("scroll-none");
+    $(".footer-container").addClass("hide");
+    $(window).on("load",function(){
+        $(".spinner-3").fadeOut("slow");
+        $("#html").removeClass("scroll-none");
+        if ($(".main-cart").length) {
+            $("#html").addClass("scroll-none");
+        }
+        else {
+            $("#html").removeClass("scroll-none");
+        }
+        $(".footer-container").removeClass("hide");
+    });
+})
+
+$(document).ready(function () {
+    $('.icon').click(function () {
+        $('.icon').toggleClass('active');
+        $('#categories-container').toggleClass('slide');
+    });
+})
+
+
